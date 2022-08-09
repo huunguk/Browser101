3e5b03b68a85222a902b5ccd03bf967a8cf59c7e
Jin HyeoungUk <huunguk@gmail.com>
2022-08-09 15:27:51 +0900

    shopping list 개선

--- Web APIs/shopping list/app.js
+++ Web APIs/shopping list/app.js
@@ -21,34 +21,24 @@ function onAdd() {
   input.focus();
 }
 
+let id = 0;
+
 function createItem(text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item_row");
-
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
-
-  const name = document.createElement("span");
-  name.setAttribute("class", "item_name");
-  name.innerText = text;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item_delete");
-  deleteBtn.innerHTML = '<i class="fa-solid fa-delete-left"></i>';
-  deleteBtn.addEventListener("click", () => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item_divider");
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.setAttribute("data-id", id);
+  itemRow.innerHTML = `
+        <div class="item">
+         <span class="item_name">${text}</span>
+           <button class="item_delete" data-id=${id}>
+              <i class="fa-solid fa-trash"></i>
+            </button>
+        </div>
+        <div class="item_divider"></div>`;
+  id++;
   return itemRow;
 }
+
 addBtn.addEventListener("click", () => {
   onAdd();
 });
@@ -56,5 +46,13 @@ addBtn.addEventListener("click", () => {
 input.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     onAdd();
+  }
+});
+
+items.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item_row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });

3d4c63ab80daf6137d16cfc54221853ecc564317
Jin HyeoungUk <huunguk@gmail.com>
2022-08-20 18:20:37 +0900

    carrot game 리팩토링

--- carrot/app.js
+++ carrot/app.js
@@ -125,7 +125,7 @@ function initGame() {
   score = 0;
   field.innerHTML = "";
   gameScore.innerText = CARROT_COUNT;
-  //벌레와 당근을 생성한 뒤 field에 추가해준다.
+  //벌레와 당근을 생성한 뒤 field에 추가해준다
   addItem("carrot", CARROT_COUNT, "img/carrot.png");
   addItem("bug", BUG_COUNT, "img/bug.png");
 }
